7e8b8de0d51953d7dcd6fc52f8454b66d1e4b625
Nicolas Urrutia <nicoxo95@gmail.com>
2024-04-04 13:49:22 -0300

    first commit

---
+++ src/assets/components/UI/navbar.tsx
@@ -0,0 +1,17 @@
+// import './App.css';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">DREAM VIEW</div>
+      <ul>
+        <li><a href="#">Destacadas</a></li>
+        <li><a href="#">Cartelera</a></li>
+        <li><a href="#">Comprar ticket</a></li>
+      </ul>
+      <div className="conectar">Conectar</div>
+    </div>
+  );
+}
+
+export default NavBar;
